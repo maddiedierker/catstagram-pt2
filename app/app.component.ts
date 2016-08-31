a1cecbe737adb3184f91d597efa433fd6ec09da4
Madelyn Kasula <madelynkasula@gmail.com>
2016-08-30 23:28:35 -0500

    Output posts with fake data from PostService

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { PostService } from '../app/service/post.service';
 
 @Component({
   selector: 'app',
-  templateUrl: './app/app.component.html'
+  templateUrl: './app/app.component.html',
+  providers: [ PostService ]
 })
 
-export class AppComponent { }
+export class AppComponent {
+  postService: PostService;
+  posts = [];
+
+  constructor(ps: PostService) {
+    this.postService = ps;
+  }
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+  }
+}
---
+++ app/service/post.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class PostService {
+  posts = [
+    {
+      "id": 1,
+      "image": "http://f3savannahcat.com/wp-content/uploads/2015/05/which-savannah-is-right-for-you.jpg",
+      "description": "savannah cat",
+      "likes": 3,
+      "iLike": true
+    },
+    {
+      "id": 2,
+      "image": "http://purrfectcatbreeds.com/wp-content/uploads/2014/06/bengal1.jpg",
+      "description": "bengal cat",
+      "likes": 10,
+      "iLike": false
+    },
+    {
+      "id": 3,
+      "image": "http://mainecoon.org/wp-content/uploads/2016/03/maine-coon-cat2.jpg",
+      "description": "maine coon",
+      "likes": 300,
+      "iLike": true
+    }
+  ];
+
+  getPosts() {
+    return this.posts;
+  }
+}
